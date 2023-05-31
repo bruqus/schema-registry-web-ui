4c031191cb827802f6800f5d378edb49c833f124
Сивцов Никита Игоревич <sivtsov_ni@open.ru>
2023-05-31 18:07:41 +0300

    feat: remove forced redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React, { useEffect, useLayoutEffect } from 'react';
+import React from 'react';
 import { ToastContainer } from 'react-toastify';
-import { useNavigate } from 'react-router';
 import 'react-toastify/dist/ReactToastify.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import AppRoutes from 'modules/AppRouter';
-import APIStore from 'modules/API/store';
-import { ROUTE_GLOBAL_CONFIG } from 'constants/routes';
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!APIStore.getBaseUrl()) {
-      navigate(ROUTE_GLOBAL_CONFIG);
-    }
-  }, []);
-
   return (
     <div className="App">
       <AppRoutes />
--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-import { ROUTE_SCHEMA_NEW } from 'constants/routes';
+import { ROUTE_GLOBAL_CONFIG, ROUTE_SCHEMA_NEW } from 'constants/routes';
+import APIStore from 'modules/API/store';
 
 const Home = () => {
+  const [configured, setConfigured] = useState(false);
+
+  useEffect(() => {
+    setConfigured(Boolean(APIStore.getBaseUrl()));
+  }, []);
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" height="100%">
       <Typography variant="body1">
@@ -14,14 +21,25 @@ const Home = () => {
         </Typography>
       </Typography>
 
-      <Typography variant="body1">
-        select a schema or{' '}
-        <Link to={ROUTE_SCHEMA_NEW}>
-          <Typography component="span" variant="inherit" color="primary">
-            create a new one
-          </Typography>
-        </Link>
-      </Typography>
+      {configured ? (
+        <Typography variant="body1">
+          select a schema or{' '}
+          <Link to={ROUTE_SCHEMA_NEW}>
+            <Typography component="span" variant="inherit" color="primary">
+              create a new one
+            </Typography>
+          </Link>
+        </Typography>
+      ) : (
+        <Typography variant="body1">
+          configure the api url on the{' '}
+          <Link to={ROUTE_GLOBAL_CONFIG}>
+            <Typography component="span" variant="inherit" color="primary">
+              global configuration page
+            </Typography>
+          </Link>
+        </Typography>
+      )}
     </Box>
   );
 };
