fdd9c53501aac3fdda7a646a11239aff30181615
Сивцов Никита Игоревич <sivtsov_ni@open.ru>
2023-06-06 09:29:58 +0300

    feat: add event constant, add version to route constant

--- src/constants/events.ts
+++ src/constants/events.ts
@@ -1,2 +1,3 @@
 export const EVENT_SCHEMA_CREATED = 'event:schema-created';
+export const EVENT_SCHEMA_UPDATED = 'event:schema-updated';
 export const EVENT_SCHEMA_DELETED = 'event:schema-deleted';
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,5 @@
 export const ROUTE_INDEX = '/';
 export const ROUTE_SCHEMA = '/schema';
 export const ROUTE_SCHEMA_NEW = ROUTE_SCHEMA + '/new';
-export const ROUTE_SCHEMA_ITEM = ROUTE_SCHEMA + '/:subject';
+export const ROUTE_SCHEMA_ITEM = ROUTE_SCHEMA + '/:subject/version/:version';
 export const ROUTE_GLOBAL_CONFIG = '/config';
